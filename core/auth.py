3d5fb97f6e57f311b9e73a8011df9b9431ccb43d
reim <essencecup@gmail.com>
2026-07-07 22:05:11 +0700

    admin

--- core/auth.py
+++ core/auth.py
@@ -90,7 +90,7 @@ def callback():
         
         if is_admin_flag:
             flash(f"Welcome back, Captain {user_name}!", "success")
-            return redirect(url_for('main.menu')) 
+            return redirect(url_for('admin.dashboard')) 
         else:
             flash(f"Logged in as {user_name}.", "success")
             return redirect(url_for('main.menu'))
